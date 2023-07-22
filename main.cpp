451e376edd8d5cde961062e00d63ea8f3f733df1
Akbar Abayev <105016091+MedicalEnvironment@users.noreply.github.com>
2023-07-22 20:22:41 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,11 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
-
-int main(int argc, char *argv[])
-{
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-#endif
-
+int main(int argc, char *argv[]) {
     QGuiApplication app(argc, argv);
-
     QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/main.qml"));
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
-    engine.load(url);
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    if (engine.rootObjects().isEmpty()) return -1;
 
     return app.exec();
 }
